c50bfd3b80f02d8c60baeca64b9f5b2616ea9ebc
Likhith SP <splikith04@gmail.com>
2025-11-26 18:41:03 +0530

    Add static file serving and initial HTML structure for Quotes API

--- quotes-api/server.js
+++ quotes-api/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -9,9 +10,14 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Quotes API is running'));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/quotes', require('./routes/quotes'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+// API routes
+app.use('/api/quotes', require('./routes/quotes'));
+app.use('/quotes', require('./routes/quotes')); // Also support /quotes endpoint
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
